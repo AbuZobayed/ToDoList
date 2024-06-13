edd2d15948ab97f3cfee402a62f8fb322b49f96b
AbuZobayed <zobayed.ict05@gmail.com>
2024-06-12 23:10:56 -1200

    savedata localStorage

--- script.js
+++ script.js
@@ -16,13 +16,27 @@ const listContainer = document.getElementById("list-container")
         li.appendChild(span);
     }
     inputBox.value = '';
+    saveData();
 }
 
 listContainer.addEventListener('click' , function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle('checked');
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
-        e.target.ParentElement.remove()
+        e.target.parentElement.remove()
+        saveData();
     }
 },false);
+
+
+const saveData = () =>{
+    localStorage.setItem("data" , listContainer.innerHTML)
+}
+
+const showList = () =>{
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showList()
